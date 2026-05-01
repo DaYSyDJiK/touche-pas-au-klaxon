3517690129f9332e2100cfc1115632304662493a
Max <maxi.gauthier112@gmail.com>
2026-05-01 13:04:48 +0200

    refonte de la page d'accueil avec boostrap

--- app/Views/trajets/index.php
+++ app/Views/trajets/index.php
@@ -1,19 +1,38 @@
-<h1>Trajets disponibles</h1>
+<h1 class="mb-4">Trajets disponibles</h1>
 
 <?php if (empty($trajets)): ?>
     <p>Aucun trajet disponible.</p>
 <?php else: ?>
-    <ul>
-    <?php foreach ($trajets as $trajet): ?>
-        <li>
-            <?= htmlspecialchars($trajet['agence_depart']) ?> →
-            <?= htmlspecialchars($trajet['agence_arrivee']) ?> <br>
-            (Départ : <?= date('d/m/Y H:i', strtotime($trajet['date_heure_depart'])) ?>) <br>
-            (Arrivée : <?= date('d/m/Y H:i', strtotime($trajet['date_heure_arrivee'])) ?>) <br>
-            Places dispo : <?= $trajet['nombre_places_disponibles'] ?>
-        </li>
-    <?php endforeach; ?>
-    </ul>
+
+    <div class="row">
+        <?php foreach ($trajets as $trajet): ?>
+
+            <div class="col-md-6 mb-4">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+
+                        <h5 class="card-title">
+                            <?= htmlspecialchars($trajet['agence_depart']) ?>
+                            →
+                            <?= htmlspecialchars($trajet['agence_arrivee']) ?>
+                        </h5>
+
+                        <p class="card-text">
+                            <strong>Départ :</strong>
+                            <?= date('d/m/Y H:i', strtotime($trajet['date_heure_depart'])) ?><br>
+
+                            <strong>Arrivée :</strong>
+                            <?= date('d/m/Y H:i', strtotime($trajet['date_heure_arrivee'])) ?><br>
+
+                            <strong>Places disponibles :</strong>
+                            <?= $trajet['nombre_places_disponibles'] ?>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+
+        <?php endforeach; ?>
+    </div>
+
 <?php endif; ?>
-
-
